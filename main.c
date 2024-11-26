fddbe316e231bb3c45602f2eec4b33c101133a95
Ashley <azhu50@stuy.edu>
2024-11-26 15:35:27 -0500

    rand numbers and wait:

--- main.c
+++ main.c
@@ -2,15 +2,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 int main(){
     pid_t fork1;
     printf("%d about to create 2 child processes\n", getpid());
     fork();
     fork1 =fork();
-    int r_file = open("/dev/random", O_RDONLY,0);
+    int r_file = open("/dev/urandom", O_RDONLY,0);
     if(fork1==0){
-        printf("%d", getpid());
+        int *p = malloc(sizeof(p));
+        read(r_file, p, sizeof(p));
+        int randnum = (*p) %5 ;
+        if(randnum<=0){
+            randnum+=5;
+        }
+        printf("%d %dsec \n", getpid(), randnum);
+        sleep(randnum);
+        printf("%d finished after %dsec\n", getpid(), randnum);
+        exit(3);
     }
-    
+    if(WIFEXITED(fork1)){
+        printf("Main Process %d is done. \n", getppid());
+    }
 }
